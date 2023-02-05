0f2a25dab70303142c7803d05a9dc773c74582a8
Andrei <mojitoboss@gmail.com>
2023-02-05 13:46:18 +0300

    new items

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js';
+import fileUpload from 'express-fileupload';
 
 const PORT = 5000;
 const MONGO_URL = 'mongodb+srv://admin:admin@cluster0.jxjn3gs.mongodb.net/?retryWrites=true&w=majority';
 const app = express();
 
 app.use(express.json());
+app.use(fileUpload({}));
 app.use('/api', router);
 
 async function startApp() {
